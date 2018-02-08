b6b1f5f1a2a0318c6b8c14bcf14c14a2bedaafe4
carotti <thomas@carotti.co.uk>
2018-02-08 17:03:15 +0000

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+module Program
+open System
+open Common.CommonTop
+open Common.CommonData
+
+[<EntryPoint>]
+let main argv =
+    /// test the initProjectLexer code
+    let test = parseLine None (WA 0u)
+    0 // return an integer exit code
