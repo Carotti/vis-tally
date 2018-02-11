0602ff8c2fb10a199a15e44c303b4c07e33e28b1
ChristopherMacca <christophermacca@hotmail.co.uk>
2018-02-11 12:05:10 +0000

    Instruction type (including flexible operand) defined

--- Program.fs
+++ Program.fs
@@ -3,9 +3,23 @@ module Program
 open System
 open CommonTop
 open CommonData
+open DP
+
+
+////////////////////////////////////////////////////////////////////////////////
+// maccth helper functions, to delete 
+let qp thing = thing |> printfn "%A"
+let qpl lst = lst |> List.map (qp)
+// maccth helper functions, to delete 
+////////////////////////////////////////////////////////////////////////////////
+
 
 [<EntryPoint>]
 let main argv =
     /// test the initProjectLexer code
     let test = parseLine None (WA 0u)
+    opCodes |> qp
+
+    
+
     0 // return an integer exit code
