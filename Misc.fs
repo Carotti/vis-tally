f08bf8caba1a23d166778d1644b1f3e101a81aa8
carotti <thomas@carotti.co.uk>
2018-02-12 13:54:49 +0000

    Starting to parse expressions with active pattern monadic matching

--- Misc.fs
+++ Misc.fs
@@ -5,11 +5,15 @@
 module Misc
     open CommonData
     open CommonLex
+    open System.Text.RegularExpressions
+    open Expecto.Flip
+    open System.Text.RegularExpressions
+    open System
 
     // Types for data declaration instructions
-    type InstrDCD = {values : uint32 list}
-    type InstrDCB = {values : byte list}
-    type InstrADR = {reg : RName ; value : uint32}
+    type InstrDCD = {Label : string ; values : uint32 list}
+    type InstrDCB = {Label : string ; values : byte list}
+    type InstrADR = {Label : string ; reg : RName}
 
     // instruction type
     type Instr =
@@ -39,14 +43,55 @@ module Misc
     /// otherwise it is Ok Parse or Error (parse error string)
     let parse (ls: LineData) : Result<Parse<Instr>,string> option =
         let (WA la) = ls.LoadAddr
-        let parseDCD suffix pCond : Result<Parse<Instr>,string> = 
-            let retval : InstrDCD = {values = [10u]}
-            Ok { 
-                PInstr = DCD retval; 
-                PLabel = ls.Label |> Option.map (fun lab -> lab, la) ; 
-                PSize = 4u; 
-                PCond = pCond 
+
+        /// Match the start of txt with pat
+        /// Return a tuple of the matched text and the rest
+        let (|RegexPrefix|_|) pat txt =
+            // Match from start, also don't care about whitespace
+            let m = Regex.Match (txt, "^[\\s]*" + pat + "[\\s]*")
+            match m.Success with
+            | true -> (m.Value, txt.Substring(m.Value.Length)) |> Some
+            | false -> None
+
+        let (|PrimExpr|_|) txt =
+            match txt with  
+            | RegexPrefix "[0-9]+" (num, rst) -> (uint32 num, rst) |> Ok |> Some
+            | RegexPrefix "[a-zA-Z][a-zA-Z0-9]*" (var, rst) ->
+                match ls.SymTab with
+                | Some symTab ->
+                    match (Map.containsKey var symTab) with
+                    | true -> (symTab.[var], rst) |> Ok |> Some
+                    | false -> "Symbol not declared" |> Error |> Some
+                | None -> "No Symbol table exists" |> Error |> Some
+            | _ -> None
+
+        let (|Expr|_|) txt =
+            match txt with 
+            | PrimExpr (Ok (num, rst)) -> (num, rst) |> Ok |> Some
+            | PrimExpr (Error x) -> x |> Error |> Some
+            | _ -> None
+
+        // Returns a list of expressions comma seperated in txt
+        let rec parseExprList txt : uint32 list =
+            match txt with
+            | Expr (exp, rst) ->
+                match rst with
+                | RegexPrefix "," (_, rst') -> exp :: (parseExprList rst')
+                | "" -> [exp]
+                | _ -> []
+            | _ -> []
+
+        let parseDCD _suffix pCond : Result<Parse<Instr>,string> = 
+            match ls.Label with
+            | Some x -> 
+                Ok { 
+                    PInstr = DCD {Label = x ; values = parseExprList ls.Operands}; 
+                    PLabel = ls.Label |> Option.map (fun lab -> lab, la) ; 
+                    PSize = 4u; 
+                    PCond = pCond 
                 }
+            | None -> Error "Missing DCD directive label"
+
 
         // Map roots to the functions which parse them
         let parseFuncs = 
