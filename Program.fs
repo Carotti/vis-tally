c77aa491fd0681a0e655e4ae75e1dcd94a7936af
carotti <thomas@carotti.co.uk>
2018-02-12 17:35:13 +0000

    Higher order active pattern monadic parsing of mathematical expressions

--- Program.fs
+++ Program.fs
@@ -10,6 +10,6 @@ let main _argv =
     let symTable : SymbolTable = Map.ofList [
                                     "foo", 10u;
                                 ]
-    let test = parseLine (Some symTable) (WA 16u) "FOO DCD foo, foo"
+    let test = parseLine (Some symTable) (WA 16u) "FOO DCD 3 - 5 + 5"
     printfn "%A" test
     0 // return an integer exit code
