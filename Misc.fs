6c492a26d40b9b3ffe091ef26bf7035cd8b1704e
carotti <thomas@carotti.co.uk>
2018-02-15 15:24:21 +0000

    Misc code for now..

--- Misc.fs
+++ Misc.fs
@@ -27,8 +27,51 @@ module Misc
     /// These opCodes do not have conditions or suffixes
     let opCodes = ["DCD";"DCB";"EQU";"FILL"]
 
+            /// Check if x can be rotated right in a 32 bit word
+    let rotateOk x =
+        let rec rotateOk' r =
+            let rotRight (x : uint32) n = (x >>> n) ||| (x <<< (32 - n))
+            match rotRight x r < 256u, r > 30 with
+            | _, true -> false
+            | true, false -> true
+            | false, false -> rotateOk' (r + 2)
+        rotateOk' 0
+
     let parse (ls: LineData) : Result<Parse<Instr>,string> option =
         let (WA la) = ls.LoadAddr // address this instruction is loaded into memory
+        
+        /// Check if x can be rotated right in a 32 bit word
+        let rotateOk x =
+            let rec validEncoding' r =
+                let rotRight (x : uint32) n = (x >>> n) ||| (x <<< (32 - n))
+                match rotRight x r < 256u, r > 30 with
+                | _, true -> sprintf "Cannot rotate '%d' within a 32 bit word" x |> Error
+                | true, false -> Ok ()
+                | false, false -> validEncoding' (r + 2)
+            validEncoding' 0
+
+        /// Check if x is small enough to be a byte
+        let byteOk x =
+            match (x |> byte |> uint32) = x with
+            | true -> Ok ()
+            | false -> sprintf "Cannot fit '%d' into 1 byte of memory" x |> Error
+
+        /// Returns an list of the evaluated expressions in txt
+        /// Validator is a function that will verify whether the 
+        /// result of each expression matches some condition
+        let rec parseExprList txt validator : Result<uint32 list, string> =
+            let exprBinder (exp, rst) =
+                Result.bind (fun () ->
+                    match rst with
+                    | RegexPrefix "," (_, rst') -> 
+                        Result.map (fun lst -> exp :: lst) (parseExprList rst' validator)
+                    | "" -> Ok [exp]
+                    | x -> sprintf "Unknown expression at '%s'" x |> Error
+                ) (validator exp)
+            match txt with
+            | Expr ls.SymTab x -> Result.bind exprBinder x
+            | x -> sprintf "Unrecognized expression list '%s'" x |> Error
+
         match ls.OpCode with
         | "DCD" -> None
         | "DCB" -> None
