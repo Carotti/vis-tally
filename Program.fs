7e897f74499511ed6703a74defd655569c0367d9
ChristopherMacca <christophermacca@hotmail.co.uk>
2018-02-16 14:36:33 +0000

    register flexOp2 parsing

--- Program.fs
+++ Program.fs
@@ -20,10 +20,12 @@ let main argv =
         "hello: ADD R1, R2, #0xf000000f";
         "ADD R2, R4, #0xf8000007";
         "ADD R20, R4, #0xf8000007";
-        "HELLO R1, R2, #0xf8000007";
+        "ADD R1, R2, R3, R4";
+        "ADD R0, R1, R2 ";
     ]
 
-    List.map (parseLine None (WA 0u)) instrLst
+    instrLst
+    |> List.map (fun instr -> (instr + "\n", parseLine None (WA 0u) instr))
     |> qpl
     |> ignore
     // let test = parseLine None (WA 0u) "ADD R1, R2,  #0xf000000f"
