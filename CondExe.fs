41830ca880d14feb96dae8e7499f5a7f0a8f5ac2
Doug Brion <db1415@ic.ac.uk>
2018-02-16 10:26:04 +0000

    Working on mem

---
+++ CondExe.fs
@@ -0,0 +1,7 @@
+module CondExe
+    open CommonData
+    open CommonLex
+
+    let conditionalExe dataPath = 
+        let n, z, c, v = (dataPath.Fl.N, dataPath.Fl.Z, dataPath.Fl.C, dataPath.Fl.V)
+        
