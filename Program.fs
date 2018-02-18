a8d8857d04ab209f412592bd7a8aaaf0a399e0e3
carotti <thomas@carotti.co.uk>
2018-02-18 14:49:19 +0000

    FILL errors much better

--- Program.fs
+++ Program.fs
@@ -11,6 +11,6 @@ module Program
         match argv with
         | [|"tests"|] -> runTestsInAssembly defaultConfig [||]
         | _ ->
-            let test = parseLine None (WA 16u) "FILL 1, #, 1"
+            let test = parseLine None (WA 16u) "FILL 1, #, 7"
             printfn "%A" test
             0 // return an integer exit code
