84ec9b1fa13f72e8501c7798708a953c07423298
ChristopherMacca <christophermacca@hotmail.co.uk>
2018-02-20 09:45:11 +0000

    proper execution REPL implemented

--- DP.fs
+++ DP.fs
@@ -147,6 +147,9 @@ module DP
     let consReg reg =
         regNames.[reg]
 
+    let consRegG reg =
+        reg |> string |> (+) "R" |> consReg
+
     /// Constructs an operand record of type `DP3Form` from registers specified as strings.
     let consDP3 rDest' rOp1' fOp2' =
         {
@@ -285,15 +288,6 @@ module DP
                 reg |> Ok |> Some
             | _ ->
                 None
-        
-        // let regCheck txt =
-        //     match Map.tryFind txt regNames with
-        //     | Some reg ->
-        //         reg |> Ok
-        //     | _ ->
-        //         txt + " is not a valid register."
-        //         |> ``Invalid register``
-        //         |> Error
 
         let (|RegCheck|_|) txt =
             // regCheck txt |> Some
--- playing.fsx
+++ playing.fsx
@@ -85,23 +85,25 @@ let qpl lst = lst |> List.map (qp)
 //                             )
     /// ARM register names
     /// NB R15 is the program counter as read
-[<Struct>]
-type RName = R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 
-             | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
+// [<Struct>]
+// type RName = R0 | R1 | R2 | R3 | R4 | R5 | R6 | R7 
+//              | R8 | R9 | R10 | R11 | R12 | R13 | R14 | R15
 
    
 
-    /// Map used to convert strings into RName values, 
-    /// includes register aliasses PC, LR, SP
-let regNames = 
-    Map.ofList [ 
-        "R0",R0 ; "R1",R1 ; "R2",R2 ; "R3",R3 ; "R4",R4 ; "R5",R5
-        "R6",R6 ; "R7",R7 ; "R8",R8 ; "R9", R9 ; "R10",R10 ; "R11",R11 ; 
-        "R12",R12 ; "R13",R13 ; "R14",R14 ; "R15",R15 ; 
-        "PC",R15 ; "LR",R14 ; "SP",R13 
-    ] 
+//     /// Map used to convert strings into RName values, 
+//     /// includes register aliasses PC, LR, SP
+// let regNames = 
+//     Map.ofList [ 
+//         "R0",R0 ; "R1",R1 ; "R2",R2 ; "R3",R3 ; "R4",R4 ; "R5",R5
+//         "R6",R6 ; "R7",R7 ; "R8",R8 ; "R9", R9 ; "R10",R10 ; "R11",R11 ; 
+//         "R12",R12 ; "R13",R13 ; "R14",R14 ; "R15",R15 ; 
+//         "PC",R15 ; "LR",R14 ; "SP",R13 
+//     ] 
 
 
 
-qp regs
+// qp regs
 // String.RegexMatch
+
+
